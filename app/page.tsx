319c70eb2e43fdcd721afe2bd94ba2797e1a4804
talisson <talissonb422@gmail.com>
2024-04-30 15:08:02 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/serach";
@@ -11,6 +12,18 @@ export default function Home() {
       </div>
       <div className="">
         <CategoryList />
+      </div>
+
+      <div className="px-5">
+        <Image
+          src="/promo-banner-01.png"
+          alt="até 30% de desconto"
+          width={0}
+          height={0}
+          sizes="100vw"
+          quality={100}
+          className="h-auto w-full"
+        />
       </div>
     </>
   );
