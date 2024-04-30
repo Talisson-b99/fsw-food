a74279e8c71e5482512f100f9b9d8d1aa7cf8848
talisson <talissonb422@gmail.com>
2024-04-30 11:15:02 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/serach";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
