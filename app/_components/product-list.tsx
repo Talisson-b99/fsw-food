9b53d629de8d6a71531806115231d843df6051e6
talisson <talissonb422@gmail.com>
2024-05-02 18:46:56 -0300

    fix: remove async from product list

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,7 +1,5 @@
 import { Prisma } from "@prisma/client";
-import { db } from "../_lib/prisma";
 import ProductItem from "./product-item";
-import Container from "./container";
 
 interface ProductListProps {
   products: Prisma.ProductGetPayload<{
@@ -15,7 +13,7 @@ interface ProductListProps {
   }>[];
 }
 
-const ProductList = async ({ products }: ProductListProps) => {
+const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex gap-4 overflow-scroll px-5 lg:px-0 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
