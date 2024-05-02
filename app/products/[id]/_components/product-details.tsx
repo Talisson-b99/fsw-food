9363ed3db595985185fc0319f250f7a3cc30ea5c
talisson <talissonb422@gmail.com>
2024-05-02 19:42:31 -0300

    feat: add cart button to product page

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -192,6 +192,11 @@ const ProductDetails = ({
         <h3 className="mb-3 px-5 font-semibold">Sucos</h3>
         <ProductList products={complementaryProducts} />
       </div>
+      <div className="mt-6 px-5">
+        <Button className="h-[45px] w-full text-sm font-semibold">
+          Adicionar à sacola
+        </Button>
+      </div>
     </div>
   );
 };
