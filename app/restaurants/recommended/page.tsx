961b69b4e7d88d2704e1690dc3158bad4b125894
talisson <talissonb422@gmail.com>
2024-05-03 19:55:43 -0300

    feat: ensure the recommended restaurants page is desktop responsive

--- app/restaurants/recommended/page.tsx
+++ app/restaurants/recommended/page.tsx
@@ -7,11 +7,11 @@ const RecommendedRestaurant = async () => {
   return (
     <>
       <Header />
-      <div className="p-5">
+      <div className="p-5 lg:px-20">
         <h2 className="mb-6 text-lg font-semibold">
           Restaurantes recomendados
         </h2>
-        <div className="flex w-full flex-col items-center space-y-6">
+        <div className="flex w-full flex-col items-center justify-items-center gap-5 space-y-6 lg:grid lg:grid-cols-3 lg:space-y-0 xl:grid-cols-4">
           {restaurants.map((restaurant) => (
             <RestaurantItem
               key={restaurant.id}
